c6f9ac3ac29a701df9f0c3171dfa11fd00474c55
Abdul Rahman Ali <gyansuccesstreasury@gmail.com>
2026-09-20 23:52:01 +0530

    feat: add dynamic user routes with path parameters and type validation

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+#Path Parameters
 
-#Home Route
-@app.get("/")
-def home():
-    return {"message": "Hellow to Fastapi"}
+#Users dynamic Route
+@app.get("/users1/{user_id1}")
+def get_users1(user_id1):
+    return {"user_id1": user_id1}
 
-#About Route
-@app.get("/about")
-def about():
-    return {"message":"This is About Page"}
+#Users dynamic Route with validation
+@app.get("/users2/{user_id2}")
+def get_users2(user_id2:int):
+    return {"user_id2": user_id2}
 
-#Users Route
-@app.get("/users")
-def users():
-    return {"message":["Ram","Shyam", "Ghanshyam"]}
+#Users dynamic Route with validation
+@app.get("/users/{user_id}")
+def get_users(user_id:str):
+    return {"user_id": user_id}
+
+
